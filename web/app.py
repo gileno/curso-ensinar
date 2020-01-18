a2160f5eb38a41004e4583c7a05f7ecf24195b3a
Gileno Filho <contato@gilenofilho.com.br>
2020-01-18 11:18:53 -0300

    hml

--- web/app.py
+++ web/app.py
@@ -14,4 +14,10 @@ def inicio():
     return render_template('inicio.html', titulo=titulo, contatos=contatos)
 
 
+@app.route('/novo-contato/')
+def novo_contato():
+    banco = dataset.connect('sqlite:///db.sqlite3')
+    return render_template('novo_contato.html')
+
+
 app.run(debug=True)
